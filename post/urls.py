902a666d1bef1188920ac942da4a713496e18922
anvan <ans.vanyan@gmail.com>
2020-11-13 18:52:28 +0300

    add new post form without saving option

--- post/urls.py
+++ post/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('post/list/', views.post_list, name='post_list'),
+    path('post/new/', views.post_new, name='post_new'),
     path('post/detail/<int:post_pk>/', views.post_detail, name='post_detail')
 
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -11,3 +12,9 @@ def post_list(request):
 def post_detail(request, post_pk):
     post = get_object_or_404(Post, pk=post_pk)
     return render(request, 'post/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'post/post_new.html', {'form': form})
+
