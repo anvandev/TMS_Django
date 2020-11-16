6ecf975c8beba881d88e97dd914eec77946969ea
anvan <ans.vanyan@gmail.com>
2020-11-16 13:11:04 +0300

    add save and edit options, add auth check

--- post/urls.py
+++ post/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('post/list/', views.post_list, name='post_list'),
     path('post/new/', views.post_new, name='post_new'),
-    path('post/detail/<int:post_pk>/', views.post_detail, name='post_detail')
+    path('post/detail/<int:post_pk>/', views.post_detail, name='post_detail'),
+    path('post/edit/<int:post_pk>/', views.post_edit, name='post_edit'),
 
 ]
--- post/views.py
+++ post/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import PostForm
+from django.utils import timezone
+from django.shortcuts import redirect
 
 
 def post_list(request):
@@ -15,6 +17,29 @@ def post_detail(request, post_pk):
 
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.created_date = timezone.now()
+            post.save()
+            return redirect('post_detail', post_pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'post/post_new.html', {'form': form})
 
+
+def post_edit(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.created_date = timezone.now()
+            post.save()
+            return redirect('post_detail', post_pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post/post_edit.html', {'form': form})
